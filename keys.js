cfcff55f1c3089e5e13a6f67c8f81f664225c385
Yobany <yobanydesign@gmail.com>
2018-12-05 22:32:02 -0500

    Set up OMDB retrieval and can now get movie information from the command-line.

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+  id: process.env.BIT_ID
+};
+
+exports.omdb = {
+  id: process.env.OMDB_ID
+};
--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 // npm install: dotenv, axios, --save node-spotify-api, moment //
 require('dotenv').config();
-var Axios = require('axios');
-var Spotify = require('node-spotify-api');
-var Moment = require('moment');
+var axios = require('axios');
+var spotify = require('node-spotify-api');
+var moment = require('moment');
 
 // load the keys file //
 var keys = require('./keys.js');
@@ -13,6 +13,9 @@ var cmdArgs = process.argv;
 // liri command will always be the second command-line argument //
 var liriCmd = cmdArgs[2];
 
+// empty variable for holding the name of a song, movie, or artist //
+var liriArg = "";
+
 // The parameter to the LIRI command may contain spaces //
 var liriArg = '';
 for (var i = 3; i < cmdArgs.length; i++) {
@@ -20,14 +23,18 @@ for (var i = 3; i < cmdArgs.length; i++) {
 }
 
 // OMDB retrievel //
-var movieName = process.argv[3];
 // request with axios to the OMDB API with the movie specified //
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// console.log to help debug against the actual URL //
-console.log(queryUrl);
+var queryUrl = "http://www.omdbapi.com/?t=" + liriArg + "&y=&plot=short&apikey=trilogy";
 // prints the movie information //
 axios.get(queryUrl).then(
-    function(response) {
-      console.log("Release Year: " + response.data.Year);
-    }
-  );
+  function(response) {
+    console.log('\x1b[36m%s\x1b[0m',`Title: ${response.data.Title}
+Year: ${response.data.Year}
+IMDB Rating: ${response.data.imdbRating}
+Rotten Tomatoes Rating: ${response.data.Ratings[1]}
+Produced in: ${response.data.Country}
+Language: ${response.data.Language}
+Plot: ${response.data.Plot}
+Actors: ${response.data.Actors}`);
+  }
+);
